9a1c39fb50447b9a72a10fd91b5ab39e7f8a5ca0
Scratchykaktus <termo.dog@bk.ru>
2022-08-08 16:12:41 +0300

    Add keyboard

--- data/config.py
+++ data/config.py
@@ -8,7 +8,7 @@ BOT_TOKEN = str(os.getenv('BOT_TOKEN'))
 
 # webhook settings
 WEBHOOK_HOST = str(os.getenv('WEBHOOK_HOST'))
-WEBHOOK_PATH = f'webhook/{BOT_TOKEN}'
+WEBHOOK_PATH = f'/webhook/{BOT_TOKEN}'
 WEBHOOK_URL = f'{WEBHOOK_HOST}{WEBHOOK_PATH}'
 
 # webserver settings
