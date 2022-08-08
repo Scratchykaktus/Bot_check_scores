fca5b83782bebcd3a5a8fd4e86c88fccef425e37
Scratchykaktus <termo.dog@bk.ru>
2022-08-08 15:32:14 +0300

    Add keyboard

--- handlers/users/score.py
+++ handlers/users/score.py
@@ -5,10 +5,19 @@ from aiogram.dispatcher.filters import Command, Text
 from utils.misc.request import req
 
 
-@dp.message_handler(Command('score') or Text(equals='Проверить баллы'))
+@dp.message_handler(Command('score'))
 async def cmd_score(message: Message):
     msg = ''
     for i, item in enumerate(req()):
         msg += f'[{i+1}] - {item[1]}\n' \
                f'Набрано баллов - {item[2]}\n\n'
+    await message.answer(msg)
+
+
+@dp.message_handler(Text(equals='Проверить баллы'))
+async def cmd_score(message: Message):
+    msg = ''
+    for i, item in enumerate(req()):
+        msg += f'[{i+1}] - {item[1]}\n' \
+               f'<b>Набрано баллов</b> - {item[2]}\n\n'
     await message.answer(msg)
