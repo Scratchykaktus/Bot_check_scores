c2df72649ec8cf6dc0e5e9aa0e9c386af81beb52
Scratchykaktus <termo.dog@bk.ru>
2022-08-08 16:49:15 +0300

    Add keyboard

--- app.py
+++ app.py
@@ -26,5 +26,5 @@ if __name__ == '__main__':
         on_startup=on_startup,
         on_shutdown=on_shutdown,
         host=WEBAPP_HOST,
-        port=WEBAPP_PORT,
+        port=WEBAPP_PORT
     )
--- data/config.py
+++ data/config.py
@@ -8,11 +8,11 @@ BOT_TOKEN = str(os.getenv('BOT_TOKEN'))
 
 # webhook settings
 WEBHOOK_HOST = str(os.getenv('WEBHOOK_HOST'))
-WEBHOOK_PATH = f'/webhook/'
+WEBHOOK_PATH = '/webhook/'
 WEBHOOK_URL = f'{WEBHOOK_HOST}{WEBHOOK_PATH}'
 
 # webserver settings
 WEBAPP_HOST = '0.0.0.0'
-WEBAPP_PORT = os.getenv('PORT', default=4000)
+WEBAPP_PORT = os.environ.get('PORT', default=4000)
 
 admins = str(os.getenv('ADMINS'))
