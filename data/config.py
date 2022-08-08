92f2cfaf3a722a8c09a5ebdf00e6dd42208fa12a
Scratchykaktus <termo.dog@bk.ru>
2022-08-08 16:42:20 +0300

    Add keyboard

--- data/config.py
+++ data/config.py
@@ -8,11 +8,11 @@ BOT_TOKEN = str(os.getenv('BOT_TOKEN'))
 
 # webhook settings
 WEBHOOK_HOST = str(os.getenv('WEBHOOK_HOST'))
-WEBHOOK_PATH = f'/webhook/{BOT_TOKEN}'
+WEBHOOK_PATH = f'/webhook/'
 WEBHOOK_URL = f'{WEBHOOK_HOST}{WEBHOOK_PATH}'
 
 # webserver settings
 WEBAPP_HOST = '0.0.0.0'
-WEBAPP_PORT = os.getenv('PORT', default=8000)
+WEBAPP_PORT = os.getenv('PORT', default=4000)
 
 admins = str(os.getenv('ADMINS'))
